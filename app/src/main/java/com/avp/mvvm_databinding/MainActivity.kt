052d2a653841037af1e6819025f11eb899ecffc3
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-03-18 22:27:02 +0700

    Demo basic Data binding

--- app/src/main/java/com/avp/mvvm_databinding/MainActivity.kt
+++ app/src/main/java/com/avp/mvvm_databinding/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.avp.mvvm_databinding
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.avp.mvvm_databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val mainDataBinding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+        val nameUser = "Anh Viet Pham"
+        mainDataBinding.nameUser = nameUser
     }
 }
