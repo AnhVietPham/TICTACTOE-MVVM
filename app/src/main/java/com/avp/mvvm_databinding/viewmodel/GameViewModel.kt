8d0774341f60e3425cd2d88c5775b173ef0a2609
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-03-28 22:54:20 +0700

    TicTacToe-MVVM+Databinding: Update GameViewModel.kt

--- app/src/main/java/com/avp/mvvm_databinding/viewmodel/GameViewModel.kt
+++ app/src/main/java/com/avp/mvvm_databinding/viewmodel/GameViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import com.avp.mvvm_databinding.model.Cell
 import com.avp.mvvm_databinding.model.Game
 import com.avp.mvvm_databinding.model.Player
+import com.avp.mvvm_databinding.utils.StringUtility
 
 class GameViewModel : ViewModel() {
     private lateinit var cells: ObservableArrayMap<String, String>
@@ -16,14 +17,20 @@ class GameViewModel : ViewModel() {
         cells = ObservableArrayMap()
     }
 
-    fun onClickedCellAt(row: Int, column: Int){
-        if (game.cells[row][column] == null){
-            game.cells[row][column] = Cell(game.currentPlayer)
-            cells
+    fun onClickedCellAt(row: Int, column: Int) {
+        if (game.cells[row][column] == null) {
+            game.currentPlayer?.let { currentPlayer ->
+                game.cells[row][column] = Cell(currentPlayer)
+            }
+            cells[StringUtility.stringFromNumbers(row, column)] = game.currentPlayer?.value
+            if (game.hasGameEnded())
+                game.reset()
+            else
+                game.switchPlayer()
         }
     }
 
-    fun getWinner() : LiveData<Player>{
+    fun getWinner(): LiveData<Player> {
         return game.winner
     }
 }
